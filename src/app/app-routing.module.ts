081de7fdf97723fa819edcc1ca6c78c5fab09171
Ashana700 <73642738+Ashana700@users.noreply.github.com>
2020-11-13 14:57:02 +0530

    aayu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { MsgComponent } from './msg/msg.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PoemsComponent } from './poems/poems.component';
+import { MemoriesComponent } from './memories/memories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path:'',
+  component: HomeComponent
+  },
+  {
+    path: 'Poems',
+    component: PoemsComponent
+  },
+  {
+    path: 'msg',
+    component: MsgComponent
+  },
+  {
+    path: 'memories',
+    component: MemoriesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PoemsComponent } from './poems/poems.component';
+import { MsgComponent } from './msg/msg.component';
+import { MemoriesComponent } from './memories/memories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PoemsComponent,
+    MsgComponent,
+    MemoriesComponent
   ],
   imports: [
     BrowserModule,
